712a8bcdc8593c18f60752c07ea4cf2b7689c0bd
cyxkl <72583322+cyxkl@users.noreply.github.com>
2022-02-03 18:03:43 -0800

    day 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,39 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let currentQuestionNumber = 0;
+  return (
+    <div className="app">
+      Trivia!
+      <Question text={data[0]["question"]["text"]} />
+      <Answer answer={"answer goes here"} />
+      <NextQuestion />
+    </div>
+  );
 }
 
 export default App;
+
+function Question(props) {
+  return (
+    <div>
+      <p>{props.text}</p>
+    </div>
+  );
+}
+
+function NextQuestion() {
+  return (
+    <div>
+      <button>Next Question</button>
+    </div>
+  );
+}
+
+function Answer(props) {
+  return (
+    <div>
+      <p>{props.answer}</p>
+    </div>
+  );
+}
